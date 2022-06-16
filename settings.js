f08961d390dcc5c4ac1572c21a1739c4f6bfbfc0
Mirz6a6auw <107535163+Mirz6a6auw@users.noreply.github.com>
2022-06-16 21:49:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
+global.ownerNumber = ['6285730118420']
+global.ownerName = 'Mirzz Ganzz'
 global.packname = 'Ichigo Kurosaki'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
